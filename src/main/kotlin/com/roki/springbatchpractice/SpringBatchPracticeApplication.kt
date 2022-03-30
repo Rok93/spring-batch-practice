4fdf180167aeb1a53cb1b5d162f128ca3dd62815
Rok93 <kr.kim@mfort.co.kr>
2022-03-30 11:55:01 +0900

    Initial commit

---
+++ src/main/kotlin/com/roki/springbatchpractice/SpringBatchPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.roki.springbatchpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBatchPracticeApplication>(*args)
+}
